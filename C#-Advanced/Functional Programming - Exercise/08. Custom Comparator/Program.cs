b76ecf2122dfd7845e569123c54dd0da7d307cf4
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:14:28 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/08. Custom Comparator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_8._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Func<int, int, int> func = new Func<int, int, int>((a, b) =>
+                 {
+                     if (a % 2 == 0 && b % 2 != 0)
+                     {
+                         return -1;
+                     }
+                     else if (a % 2 != 0 && b % 2 == 0)
+                     {
+                         return 1;
+                     }
+                     else
+                     {
+                        return a.CompareTo(b);
+                     }
+                 });
+            Comparison<int> compar = new Comparison<int>(func);
+            Array.Sort(input, compar);
+            Console.WriteLine(string.Join(" ",input));
+        }
+    }
+}
