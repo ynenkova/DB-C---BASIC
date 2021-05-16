ea8a80a631cd3a0cb3127bfded652562accf4cd3
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:14:52 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/09. List Of Predicates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_9._List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var divisors = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToList();
+            var divisibleNumbers = GetDivisibleNumbers(n, divisors);
+
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+        }
+
+        private static List<int> GetDivisibleNumbers(int n, List<int> divisors)
+        {
+            var divisibleNumbers = new List<int>();
+
+            for (int num = 1; num <= n; num++)
+            {
+                var isDivisible = true;
+
+                foreach (var d in divisors)
+                {
+                    Predicate<int> isNotDivisor = x => num % x != 0;
+
+                    if (isNotDivisor(d))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    divisibleNumbers.Add(num);
+                }
+            }
+            return divisibleNumbers;
+        }
+    }
+}
