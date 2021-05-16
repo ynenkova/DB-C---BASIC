ea8689cbec9b0882b3d93b9bd6f38eb166e28055
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:34:58 +0300

    Add files via upload

---
+++ C#-Advanced/Generics - Exercise/Box/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._Generic_Box_of_String
+{
+    public class Box<T> where T: IComparable
+    {
+        public Box(List<T> value)
+        {
+            this.Value = value;
+        }
+
+        public List<T> Value { get; set; } = new List<T>();
+
+        public void Swap(int first, int second)
+        {
+            T firstele = this.Value[first];
+            this.Value[first] = this.Value[second];
+            this.Value[second] = firstele;
+        }
+        public int Count(T element)
+        {
+            int count = 0;
+            foreach (T value in this.Value)
+            {
+                if (element.CompareTo(value)<0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (T item in this.Value)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+           return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#-Advanced/Generics - Exercise/Box/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Generic_Box_of_String
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var list = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                var input =double.Parse( Console.ReadLine());
+                list.Add(input);
+            }
+            var box = new Box<double>(list);
+
+            double element= double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Count(element));
+        }
+    }
+}
