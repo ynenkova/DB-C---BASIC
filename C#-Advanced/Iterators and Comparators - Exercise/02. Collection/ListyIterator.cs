400d02a4e8859e2e569d87299ba4ad5225be85eb
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:41:17 +0300

    Add files via upload

---
+++ C#-Advanced/Iterators and Comparators - Exercise/02. Collection/ListyIterator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> mylist;
+        private int index;
+        public ListyIterator(List<T> mylist)
+        {
+            this.MyList = mylist;
+            this.index = 0;
+        }
+        public List<T> MyList { get; set; }
+        public int Count => this.MyList.Count;
+        public void Create(List<T> mylist, T elements)
+        {
+            if (this.mylist.Count == 0)
+            {
+                this.MyList = new List<T>();
+            }
+            else
+            {
+                this.MyList.Add(elements);
+            }
+        }
+        public bool Move()
+        {
+            if (this.index < this.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+        public void Print()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(this.MyList[this.index]);
+        }
+        public bool HasNext()
+        {
+            if (this.index < this.Count - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in this.MyList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#-Advanced/Iterators and Comparators - Exercise/02. Collection/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine().Split();
+            ListyIterator<string> list = null;
+
+
+            while (command[0] != "END")
+            {
+                try
+                {
+
+                    var commandTwo = command[0];
+                    switch (commandTwo)
+                    {
+                        case "Create":
+                            List<string> elements = command.Skip(1).ToList();
+                            list = new ListyIterator<string>(elements);
+                            break;
+                        case "Move":
+                            Console.WriteLine(list.Move());
+                            break;
+                        case "Print":
+                            list.Print();
+                            break;
+                        case "HasNext":
+                            Console.WriteLine(list.HasNext());
+                            break;
+                        case "PrintAll":
+                            foreach (var item in list)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            Console.WriteLine();
+                            
+                            break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                command = Console.ReadLine().Split();
+            }
+        }
+    }
+}
