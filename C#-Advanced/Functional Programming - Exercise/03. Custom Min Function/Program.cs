d133704b9488a1f401fb1efd5fec95cb3bb5366b
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:12:46 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Func<string, int> parse = x => int.Parse(x);
+            var numbers = input.Split(" ").Select(parse).Min();
+            Console.WriteLine(numbers);
+           
+        }
+    }
+}
