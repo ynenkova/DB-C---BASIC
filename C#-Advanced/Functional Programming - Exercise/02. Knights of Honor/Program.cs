544e9684213850a2e4613a2e057bcc6fbd130dea
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:12:28 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(" ").Select(x=>"Sir"+" "+x).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
