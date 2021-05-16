48884084cde9c26c5c6d5a66954b63aa72be9c0d
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:13:49 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_6._Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var num = int.Parse(Console.ReadLine());
+            Func<int, bool> filter = n => n % num!= 0;
+            var reverse = input.Reverse().Where(filter);
+            Console.WriteLine(string.Join(" ",reverse));
+        }
+    }
+}
