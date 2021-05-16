e9bf28c99ace0c1a409363d86e79e251489af720
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:07:55 +0300

    Add files via upload

---
+++ C#-Advanced/Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Creating_Constructors;
+using System;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Person pesho = new Person(name, age);
+            
+            Console.WriteLine($"{pesho.Name} -> {pesho.Age}");
+        }
+    }
+}
