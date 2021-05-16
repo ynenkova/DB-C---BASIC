df149b11ef0b3f2daea41c824b860ebe60524350
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:39:09 +0300

    Add files via upload

---
+++ C#-Advanced/Iterators and Comparators - Exercise/06. Equality Logic/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EqualityLogic 
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (this.GetHashCode()==obj.GetHashCode())
+            {
+                return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+            if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+            
+            return 0;
+        }
+    }
+}
---
+++ C#-Advanced/Iterators and Comparators - Exercise/06. Equality Logic/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> peopleFirst = new SortedSet<Person>();
+            HashSet<Person> peopleHashSet = new HashSet<Person>();
+
+            
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var age = int.Parse(tokens[1]);
+
+                Person newPerson = new Person(name, age);
+
+                peopleFirst.Add(newPerson);
+                peopleHashSet.Add(newPerson);
+            }
+
+            Console.WriteLine(peopleFirst.Count);
+            Console.WriteLine(peopleHashSet.Count);
+        }
+    }
+}
