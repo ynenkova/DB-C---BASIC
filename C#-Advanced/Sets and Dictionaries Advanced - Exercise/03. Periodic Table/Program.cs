f397b55c5fc6aead4017767880dd0260c15622f7
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 13:53:02 +0300

    Add files via upload

---
+++ C#-Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ').ToArray();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    chemicalElements.Add(input[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ",chemicalElements));
+        }
+    }
+}
