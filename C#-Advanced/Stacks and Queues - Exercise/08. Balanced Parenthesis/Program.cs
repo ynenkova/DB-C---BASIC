10711ae91932c0f2bdba81b7dc9d3f1690a3a0c0
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 13:32:19 +0300

    Add files via upload

---
+++ C#-Advanced/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace _8.__Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charecter = Console.ReadLine();
+            var opashka = new Queue<char>();
+            if (charecter.Length % 2 == 0)
+            {
+                for (int i = 0; i < charecter.Length; i++)
+                {
+                    if (charecter.Length/2==i)
+                    {
+                        break;
+                    }
+                    opashka.Enqueue(charecter[i]);//[(      ])
+                }
+                var stack = new Stack<char>();
+                for (int j = charecter.Length/2; j < charecter.Length; j++)
+                {
+                    stack.Push(charecter[j]);
+                }
+                while (true)
+                {
+                    char first = opashka.Dequeue();
+                    char second = stack.Pop();
+                    if (first=='{'&&second!='}'|| first == '(' && second != ')'|| first == '[' && second != ']')
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    if (!stack.Any())
+                    {
+                        Console.WriteLine("YES");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            
+        }
+    }
+}
