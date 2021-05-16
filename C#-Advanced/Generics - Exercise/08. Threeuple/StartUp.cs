cbff7fc333b4833ff492361636ac7c8f0d5e67c2
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:35:54 +0300

    Add files via upload

---
+++ C#-Advanced/Generics - Exercise/08. Threeuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine().Split();
+            var firstName =$"{first[0]} {first[1]}";
+            var address = first[2];
+            var town = first[4].Length==0 ?$"{first[3]}":$"{first[3]} {first[4]}";
+
+            var second = Console.ReadLine().Split();
+            var nameDrunk = second[0];
+            var liters = int.Parse(second[1]);
+            var drunkResult = !second[2].Equals("drunk") ? "False" : "True";
+
+            var thirth = Console.ReadLine().Split();
+            var name = thirth[0];
+            var accountBalance = double.Parse(thirth[1]);
+            var bankName = thirth[2];
+
+            var firstTuple = new Tuple<string,string,string>(firstName, address, town);
+            var secondTuple = new Tuple<string, int, string>(nameDrunk, liters, drunkResult);
+            var thirthTuple = new Tuple<string, double, string>(name, accountBalance, bankName);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirthTuple);
+        }
+    }
+}
---
+++ C#-Advanced/Generics - Exercise/08. Threeuple/Tuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<TFirst, TSecond,Tthirth>
+    {
+        private TFirst itemFirst;
+        private TSecond itemSecond;
+        private Tthirth itemThirth;
+        public Tuple(TFirst item1, TSecond item2,Tthirth item3)
+        {
+            this.ItemFirst = item1;
+            this.ItemSecond = item2;
+            this.itemThirth = item3;
+        }
+        public TFirst ItemFirst { get; set; }
+        public TSecond ItemSecond { get; set; }
+        public Tthirth ItemThirth { get; set; }
+        public override string ToString()
+        {
+            return $"{this.ItemFirst} -> {this.ItemSecond} -> {this.itemThirth}";
+        }
+    }
+}
