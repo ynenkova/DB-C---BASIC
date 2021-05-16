60b5d0491fff2bec0c800716e9e23cceeef02d1d
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 13:52:16 +0300

    Add files via upload

---
+++ C#-Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> name = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                name.Add(input);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, name));
+        }
+    }
+}
