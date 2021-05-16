8f12e6f2791553c663ff36186652c9b1cb6ab734
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:13:27 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_5._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                if (command == "add")
+                {
+                    input=input.Select(y => y + 1).ToList();
+                }
+                if (command == "multiply")
+                {
+                    input = input.Select(y => y * 2).ToList();
+                }
+                if (command == "subtract")
+                {
+
+                    input = input.Select(y => y -1).ToList();
+                }
+                if (command == "print")
+                {
+                    Console.WriteLine(String.Join(" ", input));
+                }
+            }
+
+        }
+    }
+}
