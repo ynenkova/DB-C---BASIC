d3edf8f386179d0e969b66fffbaf798d73b239f5
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:12:10 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(" ").ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
