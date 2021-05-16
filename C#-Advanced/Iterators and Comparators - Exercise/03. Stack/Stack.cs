a0804b1810db79c0ca8e78e86e78f9e8a9b95d39
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:40:54 +0300

    Add files via upload

---
+++ C#-Advanced/Iterators and Comparators - Exercise/03. Stack/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> stacks;
+        public Stack(List<T> items)
+        {
+            stacks = new List<T>();
+        }
+
+        public int Count => this.stacks.Count;
+        public void Push(T item)
+        {
+            this.stacks.Add(item);
+
+        }
+        public T Pop()
+        {
+            if (this.stacks.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+            T item = this.stacks[this.stacks.Count - 1];
+            this.stacks.RemoveAt(this.stacks.Count - 1);
+            return item;
+        }
+
+        public void Push(IEnumerable<T> elements)
+        {
+            foreach (var item in elements)
+            {
+                this.Push(item);
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.stacks.Count-1; i >= 0; i--)
+            {
+                yield return this.stacks[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#-Advanced/Iterators and Comparators - Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>(new List<int>());
+
+            var cmdArgs = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (cmdArgs[0] != "END")
+            {
+                switch (cmdArgs[0])
+                {
+                    case "Push":
+                        var elements = cmdArgs.Skip(1).Select(int.Parse);
+                        stack.Push(elements);
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            stack.Pop();
+                        }
+                        catch (ArgumentException ae)
+                        {
+                            Console.WriteLine(ae.Message);
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+
+                cmdArgs = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            PrintStack(stack);
+            PrintStack(stack);
+            
+        }
+        public static void PrintStack(Stack<int>stack)
+        {
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
