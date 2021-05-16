1e1ec73e8614261781571d555e7f97fe72512fd4
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 13:53:43 +0300

    Add files via upload

---
+++ C#-Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> charecters = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!charecters.ContainsKey((char)input[i]))
+                {
+                    charecters.Add(input[i], 0);
+                }
+                charecters[input[i]] += 1;
+            }
+            foreach (var item in charecters.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
