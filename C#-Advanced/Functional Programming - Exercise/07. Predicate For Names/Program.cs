da5fe7d8a1f988297ccca3ee3e0e42be07845362
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:14:08 +0300

    Add files via upload

---
+++ C#-Advanced/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Problem_7._Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lenght = int.Parse(Console.ReadLine());
+            Console.ReadLine().Split(" ").
+                Where(x=>x.Length<=lenght).
+                ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
