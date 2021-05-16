5f124a80000eb781abdf8249f007dbf7ccce0f3f
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-16 14:07:31 +0300

    Add files via upload

---
+++ C#-Advanced/Defining Classes - Exercise/01. Define a Class Person/Family.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+   public class Family
+    {
+        public HashSet<Person> people;
+        public Family()
+        {
+            this.people = new HashSet<Person>();
+        }
+        public void AddMember(Person people)
+        {
+            this.people.Add(people);
+        }
+        public HashSet<Person> GetOldPeoples()
+        {
+            return this.people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToHashSet();
+        }
+    }
+}
---
+++ C#-Advanced/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] membersArg = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = membersArg[0];
+                int age = int.Parse(membersArg[1]);
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+            var result = family.GetOldPeoples();
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+    }
+}
