ea4cf3ddd247868231aa7d0fc5235a47194e9b21
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 14:56:02 +0300

    Add files via upload

---
+++ C#-OOP/Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassBox
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var leight = double.Parse(Console.ReadLine());
+                var weight = double.Parse(Console.ReadLine());
+                var height = double.Parse(Console.ReadLine());
+                var box = new Box(leight, weight, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.VolumeArea():f2}");
+            }
+            catch (Exception w)
+            {
+                Console.WriteLine(w.Message);
+            }
+
+        }
+    }
+}
