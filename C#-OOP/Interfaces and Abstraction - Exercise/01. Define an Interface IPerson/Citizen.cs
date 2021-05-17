e39dcc3a181782b604c42d15c6f5b1ce5520ed4d
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 16:10:02 +0300

    Add files via upload

---
+++ C#-OOP/Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birth;
+        public Citizen(string name, int age,string id,string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; }
+        
+        public int Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
