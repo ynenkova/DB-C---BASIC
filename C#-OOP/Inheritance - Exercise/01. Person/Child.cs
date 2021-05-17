59be08a95e85e01ae177e9a7a88c337b009493f8
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 14:45:35 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Exercise/01. Person/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+        public override int Age
+        {
+            get => base.Age;
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Age can't be greder then 15");
+                }
+                base.Age = value;
+            }
+        }
+
+    }
+}
---
+++ C#-OOP/Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get => name;  set => name = value; }
+        public virtual int Age
+        {
+            get => age;
+             set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cat't be negative");
+                }
+                this.age = value;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
