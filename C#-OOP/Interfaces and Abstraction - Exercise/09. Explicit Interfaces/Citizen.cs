bd24f7b2dc503f93ebf73897980f3eb12d5e2ad0
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 16:09:06 +0300

    Add files via upload

---
+++ C#-OOP/Interfaces and Abstraction - Exercise/09. Explicit Interfaces/Citizen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IResident, IPerson
+    {
+        private string name;
+        private string country;
+        private int age;
+
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public string Country { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string GetName()
+        {
+            return "Mr/Ms/Mrs ";
+        }
+        public string GetName(string name)
+        {
+            return name;
+        }
+    }
+}
---
+++ C#-OOP/Interfaces and Abstraction - Exercise/09. Explicit Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen citizen;
+            string input;
+            while ((input=Console.ReadLine())!="End")
+            {
+                var line = input.Split(" ");
+                citizen = new Citizen(line[0], line[1], int.Parse(line[2]));
+                Console.WriteLine(citizen.GetName(citizen.Name));
+                Console.Write(citizen.GetName(), citizen.GetName(citizen.Name));
+                Console.Write(citizen.GetName(citizen.Name));
+                Console.WriteLine();
+            }
+        }
+    }
+}
