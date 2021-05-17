749b152e49cf9c24bb81953215653c2a4416def5
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 14:49:08 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Exercise/03. Players and Monsters/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+   public abstract class Hero
+    {
+        private string username;
+        private int level;
+
+        protected Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public string Username { get => username;protected set => username = value; }
+        public int Level { get => level;protected set => level = value; }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+
+    }
+}
