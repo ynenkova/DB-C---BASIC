07b38fcde1d3e022658ec0312d014af89107b94b
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 14:47:00 +0300

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+   public abstract class Animal
+    {
+        private string name;
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get => name;protected set => name = value; }
+    }
+}
