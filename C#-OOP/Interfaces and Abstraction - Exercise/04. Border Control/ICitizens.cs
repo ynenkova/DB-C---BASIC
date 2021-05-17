20fe4ef21b83065eb2d33086f48899651adcad1d
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-17 16:02:42 +0300

    Add files via upload

---
+++ C#-OOP/Interfaces and Abstraction - Exercise/04. Border Control/ICitizens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+   public interface ICitizens
+    {
+        string Name { get; }
+        int Age { get; }
+        string Id { get; }
+    }
+}
---
+++ C#-OOP/Interfaces and Abstraction - Exercise/04. Border Control/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Citizens> citizens=new List<Citizens>();
+            List<Robots> robots=new List<Robots>();
+            while (true)
+            {
+                var command = Console.ReadLine().Split(" ");
+                if (command[0]=="End")
+                {
+                    break;
+                }
+                if (command.Length==3)
+                {
+                   
+                    citizens.Add(new Citizens(command[0], int.Parse(command[1]), command[2]));
+                }
+                else
+                {
+                    var rob = new Robots(command[0], command[1]);
+                    robots.Add(rob);
+                }
+            }
+            var specifiedDigits = Console.ReadLine();
+            citizens.Where(c => c.Id.EndsWith(specifiedDigits))
+           .Select(c => c.Id)
+           .ToList()
+           .ForEach(Console.WriteLine);
+            robots.Where(c => c.Id.EndsWith(specifiedDigits))
+          .Select(c => c.Id)
+          .ToList()
+          .ForEach(Console.WriteLine);
+        
+        }
+    }
+}
