4de46e3f1572551ba503a5d9b7867ccf723f6175
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-18 15:23:03 +0300

    Update Program.cs

--- C# Fundamentals/Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
+++ C# Fundamentals/Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -1,1 +1,19 @@
+using System;
 
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                char inchar = char.Parse(Console.ReadLine());
+                sum += inchar;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
