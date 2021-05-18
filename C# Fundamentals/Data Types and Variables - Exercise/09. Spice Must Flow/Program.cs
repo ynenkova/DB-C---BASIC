0bd1d4b38ec6dd54556fa19c6a3bfa854ba1f048
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-18 15:26:30 +0300

    Create Program.cs

---
+++ C# Fundamentals/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yeld = int.Parse(Console.ReadLine());
+            int collect = 0;
+            int days = 0;
+            if (yeld >= 100)
+            {
+                while (yeld >= 100)
+                {
+                    days++;
+                    collect += yeld;
+                    yeld -= 10;
+
+                }
+                collect -= (days + 1) * 26;
+                Console.WriteLine(days);
+                Console.WriteLine(collect);
+            }
+            else
+            {
+                Console.WriteLine(days);
+                Console.WriteLine(collect);
+            }
+        }
+    }
+}
