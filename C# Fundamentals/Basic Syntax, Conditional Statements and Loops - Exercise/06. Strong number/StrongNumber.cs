524fd4a3025dbff9a9e2adaa38c4e641d57fd810
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-18 15:14:37 +0300

    Create StrongNumber.cs

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number/StrongNumber.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace _6.Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int copyNumber = number;
+
+            int currentNumber = 0;
+            int factorialSum = 0;
+
+            while (copyNumber != 0)
+            {
+                currentNumber = copyNumber % 10;
+                copyNumber = copyNumber / 10;
+
+                int factorial = 1;
+
+                for (int i = 1; i <= currentNumber; i++)
+                {
+                    factorial = factorial * i;
+                }
+                factorialSum = factorialSum + factorial;
+            }
+            if (number == factorialSum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
