52594d861a6ae5a2bf89eb33e35d80938b83c1af
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-18 14:43:14 +0300

    Add files via upload

---
+++ C#-OOP/Polymorphism - Exercise/04. Wild Farm/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Animal : IAnimal
+    {
+        protected Animal(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.FoodEaten = 0;
+        }
+
+        public string Name { get; private set; }
+
+        public double Weight { get; protected set; }
+
+        public int FoodEaten { get; protected set; }
+
+        public abstract void FeedAnimals(string name, int quantity);
+
+
+        public abstract string MakeSound();
+
+        public abstract override string ToString();
+        
+    }
+}
---
+++ C#-OOP/Polymorphism - Exercise/04. Wild Farm/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WildFarm.Factory;
+using WildFarm.MammalCollection;
+
+namespace WildFarm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            string inputAnimal;
+            while ((inputAnimal=Console.ReadLine()) != "End")
+            {
+                Animal animal = AnimalsFactory.Create(inputAnimal.Split(" "));
+                animals.Add(animal);
+                Console.WriteLine(animal.MakeSound());
+
+                var inputFood = Console.ReadLine().Split(" ");
+                var foods = FoodFactory.Creater(inputFood);
+                try
+                {
+                    animal.FeedAnimals(foods.GetType().Name,foods.Quantity);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+            animals.ForEach(Console.WriteLine);
+        }
+    }
+}
