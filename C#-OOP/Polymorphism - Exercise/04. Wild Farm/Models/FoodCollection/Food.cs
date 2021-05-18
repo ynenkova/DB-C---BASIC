cc2fce49e2490be533537eb91ff958a377154cfa
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-18 14:46:50 +0300

    Add files via upload

---
+++ C#-OOP/Polymorphism - Exercise/04. Wild Farm/Models/FoodCollection/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm.FoodCollection
+{
+   public abstract class Food
+    {
+        protected Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+
+        public int Quantity { get; set; }
+    }
+}
