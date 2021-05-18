a66339db140c532cf3b64f9c156ae9ebc2e742a3
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-05-18 14:44:42 +0300

    Add files via upload

---
+++ C#-OOP/Polymorphism - Exercise/04. Wild Farm/Models/Factory/AnimalsFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.MammalCollection;
+
+namespace WildFarm.Factory
+{
+   public static class AnimalsFactory
+    {
+        public static Animal Create(string[] input)
+        {
+            string type=input[0];
+            switch (type)
+            {
+                case nameof(Owl):
+                    return new Owl(input[1], double.Parse(input[2]), double.Parse(input[3]));
+                case nameof(Hen):
+                    return new Hen(input[1], double.Parse(input[2]), double.Parse(input[3]));
+                case nameof(Cat):
+                    return new Cat(input[1], double.Parse(input[2]), input[3], input[4]);
+                case nameof(Tiger):
+                    return new Tiger(input[1], double.Parse(input[2]), input[3], input[4]);
+                case nameof(Dog):
+                    return new Dog(input[1], double.Parse(input[2]), input[3]);
+                case nameof(Mouse):
+                    return new Mouse(input[1], double.Parse(input[2]), input[3]);
+                default:
+                    throw new ArgumentException($"{type} is not a valid Animal type.");
+            }
+
+        }
+    }
+}
---
+++ C#-OOP/Polymorphism - Exercise/04. Wild Farm/Models/Factory/FoodFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.FoodCollection;
+
+namespace WildFarm.Factory
+{
+   public static class FoodFactory
+    {
+        public static Food Creater(string[]input)
+        {
+            var quantity =int.Parse( input[1]);
+            switch (input[0])
+            {
+                case nameof(Vegetable):
+                    return new Vegetable(quantity);
+                case nameof(Fruit):
+                    return new Fruit(quantity);
+                case nameof(Meat):
+                    return new Meat(quantity);
+                case nameof(Seeds):
+                    return new Seeds(quantity);
+                default:
+                    throw new ArgumentException($"{input[0]} is not a valid Food");
+            }
+        }
+    }
+}
