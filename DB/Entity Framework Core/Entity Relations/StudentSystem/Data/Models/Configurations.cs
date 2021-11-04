e7c1123a2d695e0c66ccea869c78edc67176bf15
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-11-04 10:39:48 +0200

    Add files via upload

---
+++ DB/Entity Framework Core/Entity Relations/StudentSystem/Data/Models/Configurations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+   internal class Configurations
+    {
+        internal static string ConnectionString = "Server=LAPTOP-EJ1MT1A4;Database=StudentSystem;Integrated Security=true";
+    }
+}
