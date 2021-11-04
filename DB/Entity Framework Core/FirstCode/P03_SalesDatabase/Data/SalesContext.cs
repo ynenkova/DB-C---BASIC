3489c5c18d795c73a86501aa552edb4b64950369
ynenkova <78798679+ynenkova@users.noreply.github.com>
2021-11-04 14:50:31 +0200

    Add files via upload

---
+++ DB/Entity Framework Core/FirstCode/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override  void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=LAPTOP-EJ1MT1A4;Database=SalesDatabase;Integrated Security=true");
+            }
+        }
+        
+        protected override  void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(product =>
+            {
+                product.Property(x => x.Name).IsUnicode();
+                product.Property(x => x.Description)
+                .HasDefaultValue("No description");
+            });
+            modelBuilder.Entity<Customer>(customer =>
+            {
+                customer.Property(x => x.Name).IsUnicode();
+                customer.Property(x => x.Email).IsUnicode(false);
+            });
+            modelBuilder.Entity<Store>(store =>
+            {
+                store.Property(x => x.Name).IsUnicode();
+            });
+            modelBuilder.Entity<Sale>(sale =>
+            {
+                sale.Property(x => x.Date).HasDefaultValueSql("GETDATE()");
+            });
+            modelBuilder.Entity<Sale>(sales =>
+            {
+                sales.HasOne(x => x.Product)
+                .WithMany(x => x.Sales)
+                .HasForeignKey(x => x.ProductId);
+
+                sales.HasOne(x => x.Customer)
+                .WithMany(x => x.Sales)
+                .HasForeignKey(x => x.CustomerId);
+
+                sales.HasOne(x => x.Store)
+                .WithMany(x => x.Sales)
+                .HasForeignKey(x => x.StoreId);
+            });
+        }
+    }
+}
---
+++ DB/Entity Framework Core/FirstCode/P03_SalesDatabase/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using P03_SalesDatabase.Data;
+using Microsoft.EntityFrameworkCore;
+namespace P03_SalesDatabase
+{
+   public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            var db = new SalesContext();
+            db.Database.Migrate();
+        }
+    }
+}
